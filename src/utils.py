e61013e82a37b36441620e0d498a1a2a47a9ebcc
sipho <sipholukhele1994@gmail.com>
2025-04-21 18:29:23 +0200

    added hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import (accuracy_score, f1_score, mean_absolute_error, mean_squared_error, r2_score)
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -22,7 +23,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     """
     Evaluate the model using different metrics.
     """
@@ -31,7 +32,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param=params[list(models.keys())[i]] ######################
+
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train)
 
             # Predicting the model for both train and test data
             y_train_pred = model.predict(X_train)
